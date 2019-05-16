dfd20c015ed556669714209eca3da908337386f8
Gia Tofan <yasmina_geanina@yahoo.com>
2019-05-16 20:09:39 +0300

    fix user email migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
+            $table->string('email')->unique();
             $table->string('name');
             // $table->string('email')->nullable;
             $table->timestamp('email_verified_at')->nullable();
